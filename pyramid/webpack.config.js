5f73ff7dfb5542ed653a6f386435a403b3b4cd05
boredasfawk <olonnyetaylor@gmail.com>
2019-09-07 21:24:11 -0700

    threejs migration complete
      -- installed url/file loader and add webpack rules
      --- testing code to get webpack to bundle images/textures/sounds with bundle.js

--- pyramid/webpack.config.js
+++ pyramid/webpack.config.js
@@ -5,6 +5,22 @@ const config = {
   output: {
     path: path.resolve(__dirname, "public/javascripts"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "public/images/[hash]-[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
   }
 };
 
